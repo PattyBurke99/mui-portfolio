f0cbae16820faa4d4460b52fc270a7b9b6e96a9f
PattyBurke99 <pat.burke1917@gmail.com>
2024-10-02 20:28:32 -0300

    Adding content to Projects screen

--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -1,11 +1,49 @@
 import {
-    Box
+    Box,
+    Button,
+    Divider,
+    Typography
 } from '@mui/material'
 
 import CardGroup from '../components/CardGroup';
 
 const portfolioCard = {
     title: 'Portfolio',
+    content: (
+        <Box >
+            <Typography sx={{padding: '0.4rem'}}>
+                The site you are currently viewing was designed/created entirely by me from 
+                scratch. Technologies used during the creation of this site include TypeScript,
+                React, and Material UI.
+            </Typography>
+            <Divider />
+            <Typography sx={{padding: '0.4rem'}}>
+                This site has been designed to display correctly on a variety of screen/device
+                sizes, with good preformance and some reactivity to justify the production ready
+                technologies that were used in it's creation.
+            </Typography>
+            <Divider />
+            <Typography sx={{padding: '0.4rem'}}>
+                The source code for this site can be viewed on my GitHub below
+            </Typography>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <Button sx={{
+                    marginTop: '2rem',
+                    backgroundColor: 'secondary.light',
+                    color: '#fff'
+                }}>
+                    Source
+                </Button>
+            </Box>
+        </Box>
+    )
+}
+
+const placeholderCard = {
+    title: 'Coming Soon'
 }
 
 
@@ -15,7 +53,7 @@ function Projects() {
             display: 'flex',
             flexDirection: 'column',
         }}>
-            <CardGroup cards={[portfolioCard]} />
+            <CardGroup cards={[portfolioCard, placeholderCard]} />
         </Box>
     )
 }
