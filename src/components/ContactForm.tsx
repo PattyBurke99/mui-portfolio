97f5ab93ddd6f24bf5a0cb7862ed2452fb72d9ff
PattyBurke99 <pat.burke1917@gmail.com>
2024-10-07 17:33:05 -0300

    Added form validation, visual feadback to contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,12 +5,14 @@ import {
   Card,
   CardContent,
   CardHeader,
+  CircularProgress,
   FormControl,
   Slide,
   TextField,
   Typography
  } from '@mui/material';
 import emailjs from '@emailjs/browser';
+import { z } from "zod";
 
 import { contentCardWidthMd, contentCardWidthLg } from '../constants';
 
@@ -20,12 +22,37 @@ interface IFormData extends Record<string, string>  {
   message: string;
 }
 
+enum MessageState  {
+  notSent, sending, sent
+}
+
+const FormDataSchema = z.object({
+  user_name: z.string().min(3, {
+    message: 'Name must be atleast 3 characters!'
+  }).max(20, {
+    message: 'Name must be less than 20 characters!'
+  }),
+  user_email: z.string().email({message: 'Invalid email!'}),
+  message: z.string()
+});
+
+//Todo: change to react-hook-form or similar library for managing form state
+//Did not initally realize how complicated managing the form state would become
+
 function ContactForm() {
     const [formData, setFormData] = useState<IFormData>({
       user_name: '',
       user_email: '',
       message: ''
     });
+
+    const [formErrors, setFormErrors] = useState<IFormData>({
+      user_name: '',
+      user_email: '',
+      message: ''
+    });
+
+    const [messageState, setMessageState] = useState<MessageState>(MessageState.notSent);
 
     const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
       setFormData({
@@ -50,44 +77,89 @@ function ContactForm() {
 
     const submitForm = async (e: any) => {
         e.preventDefault();
-    
+
+        try {
+          FormDataSchema.parse(formData);
+        } catch (e: any) {
+          if (e instanceof z.ZodError) {
+            const newErrors: Record<string, string> = {};
+            e.errors.forEach((error) => {
+              newErrors[error.path[0]] = error.message;
+            });
+            setFormErrors({...formErrors, ...newErrors});
+            console.log(newErrors);
+          }
+          return;
+        }
+
+        setMessageState(MessageState.sending);
+        setFormErrors({      
+          user_name: '',
+          user_email: '',
+          message: ''
+        });
+
         const res = await emailjs.send('service_z0d8ozo', 'template_g57cmfl', formData, {
             publicKey: 'Qfhft5cghRNZ17ZvS',
         })
 
+        setMessageState(MessageState.sent);
+
         console.log(res);
+        //Todo: error handling based on res object
       };
 
     return (
         <Slide in direction="right" timeout={600}>
           <Card elevation={3} sx={{
-            width: { md: 2*parseInt(contentCardWidthMd), lg: 2*parseInt(contentCardWidthLg)},
+            width: { md: 1.5*parseInt(contentCardWidthMd), lg: 2*parseInt(contentCardWidthLg)},
           }}>
             <CardHeader title={
               <Box sx={{position: 'relative'}}>
-                  <Typography display="inline" variant="h5">Contact Me</Typography>
+                <Typography display="inline" variant="h5">Contact Me</Typography>
               </Box>
             }/>
-            <CardContent>
-              <FormControl fullWidth sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '1rem'
-              }}>
+            <CardContent sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1rem'
+            }}>
+              <FormControl 
+                fullWidth
+                error={!!formErrors.user_name}
+              >
                 <TextField 
                   id="name"
                   label="Name"
                   variant="outlined"
                   size='small'
                   onChange={onNameChange}
+                  error={!!formErrors.user_name}
+                  helperText={formErrors.user_name}
+                  value={formData.user_name}
+                  disabled={messageState === MessageState.sent}
                 />
+              </FormControl>
+              <FormControl 
+                fullWidth
+                error={!!formErrors.user_email}
+              >
                 <TextField 
                   id="email"
                   label="Email"
                   variant="outlined"
                   size='small' 
                   onChange={onEmailChange}
+                  error={!!formErrors.user_email}
+                  helperText={formErrors.user_email}
+                  value={formData.user_email}
+                  disabled={messageState === MessageState.sent}
                 />
+              </FormControl>
+              <FormControl 
+                fullWidth
+                error={!!formErrors.message}
+              >
                 <TextField 
                   id="message"
                   label="Message"
@@ -95,9 +167,31 @@ function ContactForm() {
                   multiline
                   rows={6}
                   onChange={onMessageChange}
+                  error={!!formErrors.message}
+                  helperText={formErrors.message}
+                  value={formData.message}
+                  disabled={messageState === MessageState.sent}
                 />
-                <Button onClick={submitForm}>Submit</Button>
               </FormControl>
+              {(messageState !== MessageState.sent) &&
+              <Button onClick={submitForm} sx={{
+                backgroundColor: 'secondary.light',
+                color: '#fff',
+              }}>
+                {(messageState === MessageState.notSent) ? 'Send Message'
+                : <CircularProgress size="1.5rem"/>}
+              </Button>}
+              {(messageState === MessageState.sent) &&
+                <Button
+                  disabled
+                  sx={{
+                    backgroundColor: 'success.light',
+                    color: '#fff'
+                  }}
+                >
+                  Message Sent!
+                </Button>
+              }
             </CardContent>
           </Card>
         </Slide>
