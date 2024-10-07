23f2cd03f91f6cb7be1052b53831230db1ea5648
PattyBurke99 <pat.burke1917@gmail.com>
2024-10-07 16:31:27 -0300

    Removed Contact.tsx screen (unneeded)

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ import '@fontsource/roboto/700.css';
 import Layout from "./screens/Layout";
 import Home from "./screens/Home";
 import Projects from "./screens/Projects";
-import Contact from "./screens/Contact";
+// import Contact from "./screens/Contact";
+import ContactForm from "./components/ContactForm";
 
 const router = createBrowserRouter([
   {
@@ -33,7 +34,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/mui-portfolio/contact",
-        element: <Contact />
+        element: <ContactForm />
       }
     ]
   },
