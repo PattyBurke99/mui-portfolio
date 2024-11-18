906f1f18db97f474805a0a8fb775d157fc12b5e3
PattyBurke99 <pat.burke1917@gmail.com>
2024-11-18 16:44:50 -0400

    Added new headshot to portfolio

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -19,7 +19,7 @@ function About() {
             flexDirection: 'column',
             height: 'calc(100% - 3rem)'
         }}>
-            <Avatar sx={{
+            <Avatar src='https://patrickburke.dev/Headshot.jpeg' sx={{
                 width: { xs: '192px', md: '128px', xl: '256px' },
                 height: { xs: '192px', md: '128px', xl: '256px' },
                 margin: 'auto',
